51b85c21741ed62b809104885526e8d09bfb96ca
weilannw <nickww718@gmail.com>
2018-03-24 00:02:24 -0400

    added method declarations for tagger

--- src/tagger/tagger.h
+++ src/tagger/tagger.h
@@ -2,6 +2,10 @@
 #define tagger_h
 
 typedef struct word_tag_pair_t{
-    
+    int word_id; // index of the word in the corpus
+    int tag;
 }word_tag_pair_t;
+
+int apply_tag(int, int);
+char * output_tagged_text(int, int);
 #endif
