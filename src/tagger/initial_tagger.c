485b360339dd8ffdae037fdcadfbc02ff5691373
Colton Simmons <Colton@Coltons-MacBook-Pro.local>
2018-03-24 18:52:50 -0400

    added memory map line processing

--- src/tagger/initial_tagger.c
+++ src/tagger/initial_tagger.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include "../dictionary/dictionary_generator.h"
+#include "tags.h"
+#include "../lib/hashmap.h"
+#include <strings.h>
 
+
+void process_line(char* mem_map_line, map_t map){
+    //memory map is a pointer to the beginning of the line in the memory map
+    int hashed_value;
+    
+    if(hashmap_get(mymap, word, (void **)&hashed_value) == MAP_MISSING){
+        //TO-DO:
+        //Send most common hashed tag value.
+    }
+    else{
+        if(apply_tag(hashed_value, mem_map_line)==-1) exit(0);
+    }
+    
+    
+}
