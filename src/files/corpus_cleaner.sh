91051258f39e3e3e5b63a448b0ee8b6818c11bd4
Nicholas Weiland <nickww718@gmail.com>
2018-04-15 15:01:42 -0400

    updated file editing script -- remove '' from sed command after -i for it to work with non-mac sed

--- src/files/corpus_cleaner.sh
+++ src/files/corpus_cleaner.sh
@@ -1,78 +1,31 @@
 #!/bin/bash
 # Author: Nicholas Weiland
-# Sorry about this
 # adds tag padding space, removes garbage data, and only keeps
 # the useful columns --> [word] [tag]
 CORPUSFILE=$1
-awk -F "[_\r\n]" '
-BEGIN {
-	bad_lines = 0
-	indicator = "NULL\tNULL\tNULL\t!#$&*()_+"
-				#hash for !#$&*()_+ is -759758147 
-}
-{ 
-	# sometimes patterns of 11 lines of @ symbols occur. These
-	# are deleted and replaced with a one-line 
-	# special indicator string. If the pattern is detected, 
-	# bad_lines is 10. When bad_lines is not 0, 
-	# decrement and skip the line, otherwise print the line.
-	# html elements are also replaced with
-	# the special string. These garbage words are usually tagged with null.
-	if (bad_lines > 1)
-	 	bad_lines--
-	else if (bad_lines == 1){
-		bad_lines--
-		print indicator
-	}
-	else if (pattern_occurred())
-	 	bad_lines = 10
-	else if (current_line_is_garbage())
-		print indicator
-	else
-		print $1 
-}
-# Checks if current and next 10 lines are @\tii lines.
-# For some reason this pattern appears a lot and 
-# I have no idea what it means. Very annoying.
-function pattern_occurred(){
-    if(!atsign_line($0))
-        return 0
-	else{
-		# gets the next 10 lines and outputs them delimited by newline
-        sedcmd = "RESULTX=\"$(sed -n '"'"'" FNR + 1 "," FNR + 10 "p;" FNR + 11 "q'"'"' " FILENAME "; echo x)\"; RESULT=\"${RESULTX%x}\"; printf \"%s\" \"$RESULT\"; "
-		for(i = 0; i < 10; i++){
-            sedcmd | getline nextline
-            if (!atsign_line(nextline)){
-                close(sedcmd)
-                return 0
-            }
-		}
-		#if it got to this return, there were 10 @\tii lines
-		close(sedcmd)
-		return 1
-	}
-}
-#checks if word is @ symbol
-function atsign_line(line){
-    len = split(line, wlp, "\t")
-	return len >= 4 && wlp[3] == "@"
-}
-#detects html lines
-function current_line_is_garbage(){
-	len = split($0, wlp, "\t")
-	return (len < 4 || wlp[4] == null && substr(wlp[3], 0, 1) == "<")
-}
-' $CORPUSFILE | awk -F "\t" 'BEGIN {
-	spaces = "     "
-	substring = " "
-	endind = 0
+DESTFILE=$2
+PADDINGLEN=$3
+PADDING=''
+SEDPADDING=''
+for (( i=0; i<${PADDINGLEN}; i++ ))
+do PADDING+=' '
+done
+for (( i=2; i<${PADDINGLEN}; i++ ))
+do SEDPADDING+=' '
+done
+awk -F "[_\r\n]" '{ print $1 }' "$CORPUSFILE" |
+awk -F "\t" 'BEGIN {
+	paddinglen = '${PADDINGLEN}'
+	padding = "'"$PADDING"'"
+	tag = ""
+	taglen = 0
 }
 {
-	endind = end_index($NF)
-	substring = substr($NF, 0, endind)
-	printf("%s\t%s%.*s\n", $3, substring, 5-endind, spaces);
+	taglen = end_index($NF)
+	tag = substr($NF, 0, taglen)
+	printf("%s\t%s%.*s\n", $3, tag, paddinglen-taglen, padding)
 }
-# finds either last index, or @ symbol
+# finds either last index, or @ symbol index
 function end_index(TAG){
 	len = length(TAG)
 	ind = index(TAG, "@")
@@ -80,4 +33,8 @@ function end_index(TAG){
 	return ind-1
 	return len
 }
-'
+' > "$DESTFILE"
+sed -i '' '/@	ii'"$SEDPADDING"'$/{
+        N;N;N;N;N;N;N;N;N;N;
+        s/@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'\n@	ii'"$SEDPADDING"'/@	null /   
+     }' "$DESTFILE"
