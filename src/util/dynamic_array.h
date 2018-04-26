60945607a645bb01b24012a08c8d34a0b6dc4b85
Nicholas Weiland <nickww718@gmail.com>
2018-04-26 12:31:58 -0400

    .

--- src/util/dynamic_array.h
+++ src/util/dynamic_array.h
@@ -4,7 +4,7 @@ typedef struct dynamic_array_t{
     size_t current_length;
     size_t current_index;
     uint8_t size_of_elem;
-    void *elems;
+    void **elems;
 }dynamic_array_t;
 
 void initialize_dynamic_array(dynamic_array_t*, size_t, uint8_t);
